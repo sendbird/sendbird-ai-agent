77ffd2d5e030f759e52cf60f0d06f27a92ee7d60
Irene Ryu <irene.ryu@sendbird.com>
2025-03-28 16:28:46 +0900

    Execute scrollIntoView on DOM mount

--- sample/src/hooks/useScrollIntoView.ts
+++ sample/src/hooks/useScrollIntoView.ts
@@ -2,12 +2,32 @@ import { useEffect } from 'react';
 
 export const useScrollIntoView = () => {
   useEffect(() => {
-    if (window.location.hash) {
-      const id = window.location.hash.substring(1);
+    if (!window.location.hash) return;
+
+    const id = window.location.hash.substring(1);
+    const scrollToElement = () => {
       const element = document.getElementById(id);
       if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
       }
-    }
+    };
+
+    // Check if the DOM is ready
+    const observer = new MutationObserver((_, observer) => {
+      const element = document.getElementById(id);
+      if (element) {
+        scrollToElement();
+        observer.disconnect(); // If the element is found, stop the observer
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+
+    // If the element already exists, execute it immediately
+    scrollToElement();
+    return () => observer.disconnect();
   }, []);
 };
